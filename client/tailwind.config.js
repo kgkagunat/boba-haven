68e65b1d70fdeb0323f228548f1210ed4f0f387e
kgkagunat <kgkagunat@gmail.com>
2023-08-24 17:10:10 -1000

    add tailwind postcss config

---
+++ client/tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        'twinkle': ['Twinkle Star', 'cursive'],
+        'gamja': ['Gamja Flower', 'cursive'],
+      },
+      keyframes: {
+        subtleBounce: {
+          '0%': { transform: 'translateY(0)' },
+          '20%': { transform: 'translateY(0)' },
+          '40%': { transform: 'translateY(-8px)' },
+          '50%': { transform: 'translateY(0)' },
+          '60%': { transform: 'translateY(-5px)' },
+          '80%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(0)' }
+        }
+      },
+      animation: {
+        subtleBounce: 'subtleBounce 1s forwards'
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
