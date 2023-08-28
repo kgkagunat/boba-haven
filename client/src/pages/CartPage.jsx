244ffda706487a7e1ac7b3443d6234677a285664
kgkagunat <kgkagunat@gmail.com>
2023-08-27 17:06:23 -1000

    styled cart page

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,22 +1,24 @@
-import { CheckIcon, ClockIcon } from '@heroicons/react/20/solid'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import purpleFlowerFrame from '../assets/images/purple_flower_frame_pngwing.png'
 
+// This const `drinks` will come from the backend
 const drinks = [
   {
    
-  },
-  {
-  
   },
   // More products...
 ]
 
 function Cart() {
   return (
-    <div className="bg-white">
+    <div className="min-h-screen min-w-screen justify-center bg-gradient-to-r from-white to-purple-400">
+
+        {/* SVG Overlay */}
+        <img src={purpleFlowerFrame} alt="purple flower frame png from pngwing.com" className="absolute w-screen h-screen z-5 opacity-5 pointer-events-none" />
+
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:px-0">
-        <h1 className="text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Your Orders</h1>
+        <h1 className="font-twinkle text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Your Orders</h1>
 
         <form className="mt-12">
           <section aria-labelledby="cart-heading">
@@ -72,30 +74,34 @@ function Cart() {
 
             <div>
               <dl className="space-y-4">
-                <div className="flex items-center justify-between">
-                  <dt className="text-base font-medium text-gray-900">Subtotal</dt>
-                  <dd className="ml-4 text-base font-medium text-gray-900">$15.00</dd>
+                <div className="flex items-center justify-between font-gamja text-2xl">
+                  <dt className="text-gray-900">Subtotal</dt>
+                  <dd className="ml-4 text-gray-900">$15.00</dd>
                 </div>
               </dl>
-              <p className="mt-1 text-sm text-gray-500">Shipping and taxes will be calculated at checkout.</p>
+              <p className="mt-1 font-gamja text-xl text-gray-500">Shipping and taxes will be calculated at checkout.</p>
             </div>
 
             <div className="mt-10">
               <button
                 type="submit"
-                className="w-full rounded-md border border-transparent bg-indigo-600 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+                className="font-gamja text-2xl w-full rounded-md border border-transparent bg-indigo-600 px-4 py-3 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
               >
                 Checkout
               </button>
             </div>
 
-            <div className="mt-6 text-center text-sm">
+            <div className="font-gamja text-xl mt-6 text-center">
               <p>
                 or
-                <Link to="/" className="font-medium text-indigo-600 hover:text-indigo-500">
-                    Continue order
+
+                <div className="hover:underline">
+                <Link to="/" className="font-gamja text-2xl mt-5 text-indigo-600 hover:text-indigo-500">
+                    Continue order / back to drinks
                   <span aria-hidden="true"> &rarr;</span>
                 </Link>
+                </div>
+                
               </p>
             </div>
           </section>
