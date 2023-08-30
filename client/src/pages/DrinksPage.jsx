02db681a24a7e470eb251771e2ffae13a8a9e6bf
kgkagunat <kgkagunat@gmail.com>
2023-08-29 21:57:23 -1000

    changed drinks page

--- client/src/pages/DrinksPage.jsx
+++ client/src/pages/DrinksPage.jsx
@@ -1,1 +1,23 @@
-// Map through each individual drink and render a DrinkCard component for each one
+import React from 'react';
+import { useDrinkState } from '../utils/DrinkContext';
+
+const DrinksPage = () => {
+    const { selectedDrink } = useDrinkState();
+
+    if (!selectedDrink) {
+        return <div>Drink not found!</div>;
+    }
+
+    const { name, description, image, price } = selectedDrink;
+
+    return (
+        <div>
+            <img src={image} alt={name} />
+            <h2>{name}</h2>
+            <p>{description}</p>
+            <p>{price}</p>
+        </div>
+    );
+};
+
+export default DrinksPage;
