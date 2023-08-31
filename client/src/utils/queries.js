f5bd3e9e48b20bc3cdcb79d702701ea892236f94
kgkagunat <kgkagunat@gmail.com>
2023-08-31 13:32:41 -1000

    add query get single user and me

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,11 +1,35 @@
 // Temp example for now, until I get full back-end stuff
 import gql from 'graphql-tag';
 
-export const GET_USER = gql`
-  query GetUser($id: ID!) {
+// 
+export const GET_SINGLE_USER = gql`
+  query GetSingleUser($id: ID!) {
     user(id: $id) {
       id
       email
+      username
+      drinkOrders {
+        id
+        drinkName
+        quantity
+        date
+      }
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      id
+      email
+      username
+      drinkOrders {
+        id
+        drinkName
+        quantity
+        date
+      }
     }
   }
 `;
