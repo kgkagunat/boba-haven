7698f0da28bc0ed38606b4f382182b5e2cdeb6f0
kgkagunat <kgkagunat@gmail.com>
2023-08-31 12:07:59 -1000

    initialized apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,23 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import MainContent from './pages/MainContent';
 import { DrinkProvider } from './utils/DrinkContext';
 
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'YOUR_GRAPHQL_API_ENDPOINT', // Replace URI with our GraphQL API endpoint once available
+  cache: new InMemoryCache()
+});
+
 function App() {
   return (
-    <Router>
-      <DrinkProvider>
-        <MainContent />
-      </DrinkProvider>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <DrinkProvider>
+          <MainContent />
+        </DrinkProvider>
+      </Router>
+    </ApolloProvider>
   );
 }
 
