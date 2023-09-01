d8d015cb6446cf52081d84a040a4586834884cfe
kgkagunat <kgkagunat@gmail.com>
2023-08-31 14:28:33 -1000

    add auth provider, fixed bug

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import MainContent from './pages/MainContent';
 import { DrinkProvider } from './utils/DrinkContext';
-
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { AuthProvider } from './utils/AuthContext';
 
-// Create an HTTP link to your GraphQL API
 const httpLink = createHttpLink({
-  uri: 'YOUR_GRAPHQL_API_ENDPOINT', // Replace with your GraphQL API endpoint
+  uri: 'YOUR_GRAPHQL_API_ENDPOINT',
 });
 
-// Create auth link which adds the JWT token to every request (if it exists)
 const authLink = setContext((_, { headers }) => {
-  // Get the JWT token from local storage
   const token = localStorage.getItem('jwt');
   
-  // Return headers with the authorization (or without if no token)
   return {
     headers: {
       ...headers,
@@ -25,7 +21,6 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-// Initialize Apollo Client with the concatenated auth and HTTP links
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache()
@@ -34,11 +29,13 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <DrinkProvider>
-          <MainContent />
-        </DrinkProvider>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <DrinkProvider>
+            <MainContent />
+          </DrinkProvider>
+        </Router>
+      </AuthProvider>
     </ApolloProvider>
   );
 }
