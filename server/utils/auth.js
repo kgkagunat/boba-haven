5d8c1bd94fa18cc6b79376b19ad0574d911a5c75
Jose Seto <joseseto8@gmail.com>
2023-08-31 18:57:57 -0700

    Added from JWT branch to current branch

---
+++ server/utils/auth.js
@@ -0,0 +1,46 @@
+/* const jwt = require('jsonwebtoken');
+
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) =>{
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
+
+module.exports = authenticateToken() */
+
+const jwt = require('jsonwebtoken');
+
+const expiration = '2h';
+
+module.exports = {
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
+    return jwt.sign({ data: payload }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: expiration });
+  },
+};
