f1ced96b769232bb8ceffb1ff8bff8cf1490dae6
kgkagunat <kgkagunat@gmail.com>
2023-09-01 10:04:56 -1000

    add user & drinks property data

--- server/models/Orders.js
+++ server/models/Orders.js
@@ -1,19 +1,38 @@
 const mongoose = require('mongoose');
-
 const { Schema } = mongoose;
 
 const ordersSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
   purchaseDate: {
     type: Date,
     default: Date.now
   },
   drinks: [
     {
-      type: Schema.Types.ObjectId,
-      ref: 'Drink'
+      drink: {
+        type: Schema.Types.ObjectId,
+        ref: 'Drink'
+      },
+      quantity: {
+        type: Number,
+        required: true
+      },
+      size: {
+        type: String,
+        enum: ['small', 'medium', 'large'],
+        required: true
+      },
+      priceAtOrderTime: {
+        type: Number,
+        required: true
+      }
     }
   ]
-});
+}, { timestamps: true });
 
 const Orders = mongoose.model('Orders', ordersSchema);
 
