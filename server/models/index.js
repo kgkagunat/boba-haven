2df48cf2996b36148ee4645cc2f2f56035d3b4be
kgkagunat <kgkagunat@gmail.com>
2023-09-01 09:50:50 -1000

    removed Size model import

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,5 @@
 const User = require('./User');
 const Drink = require('./Drink');
-const Size = require('./Size');
 const Orders = require('./Orders');
 
 module.exports = { User, Drink, Size, Orders };
