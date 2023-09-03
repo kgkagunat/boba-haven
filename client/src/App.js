8fac0a64b451f6a7284ff094718bd98db5663566
kgkagunat <kgkagunat@gmail.com>
2023-09-02 22:11:05 -1000

    changed to .getItem('token')

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ const httpLink = createHttpLink({
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('jwt');
+  const token = localStorage.getItem('token');
   
   return {
     headers: {
