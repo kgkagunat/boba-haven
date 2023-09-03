fa5194f924d9e00284670bb55b7ecb89cb0f5a46
kgkagunat <kgkagunat@gmail.com>
2023-09-02 23:18:11 -1000

    added 'logout' function

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -89,21 +89,42 @@ function ProfilePage() {
                           leaveTo="transform opacity-0 scale-95"
                         >
                           <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {userNavigation.map((item) => (
-                              <Menu.Item key={item.name}>
-                                {({ active }) => (
-                                  <a
-                                    href={item.href}
-                                    className={classNames(
-                                      active ? 'bg-gray-100' : '',
-                                      'block px-4 py-2 text-sm text-gray-700'
-                                    )}
-                                  >
-                                    {item.name}
-                                  </a>
-                                )}
-                              </Menu.Item>
-                            ))}
+                          {userNavigation.map((item) => (
+                            <Menu.Item key={item.name}>
+                              {({ active }) => {
+                                if (item.name === 'Sign out') {
+                                  return (
+                                    <a
+                                      href={item.href}
+                                      onClick={(e) => {
+                                        e.preventDefault(); 
+                                        item.action(); 
+                                        window.location.href = item.href; // Redirect to the home page after logging out
+                                      }}
+                                      className={classNames(
+                                        active ? 'bg-gray-100' : '',
+                                        'block px-4 py-2 text-sm text-gray-700'
+                                      )}
+                                    >
+                                      {item.name}
+                                    </a>
+                                  );
+                                } else {
+                                  return (
+                                    <a
+                                      href={item.href}
+                                      className={classNames(
+                                        active ? 'bg-gray-100' : '',
+                                        'block px-4 py-2 text-sm text-gray-700'
+                                      )}
+                                    >
+                                      {item.name}
+                                    </a>
+                                  );
+                                }
+                              }}
+                            </Menu.Item>
+                          ))}
                           </Menu.Items>
                         </Transition>
                       </Menu>
