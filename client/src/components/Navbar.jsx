275c30d3c8c7e1ca4c0fe0a0ebb39fd78c782351
kgkagunat <kgkagunat@gmail.com>
2023-09-03 12:19:37 -1000

    add modal & logic for accessing cart

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import AuthService from '../utils/auth';
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   const { data } = useQuery(GET_ME);
 
@@ -36,7 +37,11 @@ function Navbar() {
               <FontAwesomeIcon icon={faQuestionCircle} className="icon-1.4x p-1" />
           </Link>
           {/* Cart links */}
-          <Link to="/cart" className="hover:text-pink-400 transform transition-transform duration-200 hover:scale-150">
+          <Link 
+              to={currentUser ? "/cart" : "#"} 
+              onClick={() => !currentUser && setShowModal(true)}
+              className="hover:text-pink-400 transform transition-transform duration-200 hover:scale-150"
+            >
               <FontAwesomeIcon icon={faShoppingCart} className="icon-1.4x p-1" />
           </Link>
         </div>
@@ -85,9 +90,14 @@ function Navbar() {
             <FontAwesomeIcon icon={faQuestionCircle} className="icon-1.4x p-2" />
         </Link>
         {/* Cart Hamburger */}
-        <Link to="/cart" className="block px-2 py-1 text-black hover:text-gray-800 transform transition-transform duration-200 hover:scale-150">
+        <Link 
+            to={currentUser ? "/cart" : "#"} 
+            onClick={() => !currentUser && setShowModal(true)}
+            className="block px-2 py-1 text-black hover:text-gray-800 transform transition-transform duration-200 hover:scale-150"
+          >
             <FontAwesomeIcon icon={faShoppingCart} className="icon-1.4x p-2" />
         </Link>
+
         {/* Login Hamburger */}
         {currentUser ? (
           <button onClick={logout} className="block px-2 py-1 text-black hover:text-gray-800 transform transition-transform duration-200 hover:scale-150">
@@ -101,6 +111,44 @@ function Navbar() {
           </Link>
         )}
       </div>
+
+      {/* Modal */}
+      {
+        showModal && (
+          <div className="fixed z-10 inset-0 overflow-y-auto">
+            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+              <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+                <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+              </div>
+              <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+              <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                <div>
+                  <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
+                    <svg className="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                  </div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <h3 className="text-lg leading-6 font-medium text-gray-900">
+                      Access Denied
+                    </h3>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        You must be logged in to access the cart.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-6">
+                  <button onClick={() => setShowModal(false)} type="button" className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm">
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      }
     </nav>
   );
 }
