aa444ba7e7eeac0e8a8c197c3870477b899fe19c
kgkagunat <kgkagunat@gmail.com>
2023-09-04 13:11:13 -1000

    changed href

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -11,7 +11,7 @@ const navigation = {
     social: [
       {
         name: 'Facebook',
-        href: '#',
+        href: '/404',
         icon: (props) => (
           <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
             <path
@@ -24,7 +24,7 @@ const navigation = {
       },
       {
         name: 'Instagram',
-        href: '#',
+        href: '/404',
         icon: (props) => (
           <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
             <path
@@ -37,7 +37,7 @@ const navigation = {
       },
       {
         name: 'Twitter',
-        href: '#',
+        href: '/404',
         icon: (props) => (
           <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
             <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
@@ -46,7 +46,7 @@ const navigation = {
       },
       {
         name: 'YouTube',
-        href: '#',
+        href: '/404',
         icon: (props) => (
           <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
             <path
@@ -64,38 +64,40 @@ function Footer() {
     return (
       <footer className="bg-gradient-to-b from-white to-purple-200">
       <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
-          {/* Footer Nav Links */}
-          <nav className="-mb-10 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
+        <div className="flex flex-col items-center sm:flex-row sm:justify-center sm:space-x-12" aria-label="Footer">
           {navigation.main.map((item) => (
-              <div key={item.name} className="pb-6 hover:animate-subtleBounce">
-              <Link to={item.href} className="font-twinkle text-2xl leading-6 text-gray-600 hover:text-pink-400 file:">
-                  {item.name}
-              </Link>
-              </div>
+            <Link
+              key={item.name}
+              to={item.href}
+              className="font-twinkle text-2xl leading-6 text-gray-600 hover:text-pink-400 p-1 -m-2 hover:animate-subtleBounce"
+            >
+              {item.name}
+            </Link>
           ))}
-          </nav>
-          {/* Footer Social Nav Links */}
-          <div className="mt-10 flex justify-center space-x-10">
+        </div>
+        <div className="mt-10 flex justify-center space-x-10">
           {navigation.social.map((item) => (
-              <a key={item.name} href={item.href} className="text-gray-600 hover:text-pink-400 transform transition-transform duration-300 hover:scale-150">
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-gray-600 hover:text-pink-400 transform transition-transform duration-300 hover:scale-150"
+            >
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-8 w-8" aria-hidden="true" />
-              </a>
+            </a>
           ))}
-          </div>
-          <p className="font-gamja mt-6 text-center text-xl leading-5 text-pink-500">
+        </div>
+        <p className="font-gamja mt-6 text-center text-xl leading-5 text-pink-500">
           &copy; 2023 Boba Haven, Inc. All rights reserved.
-          </p>
-          <p className="font-gamja mt-5 text-center text-xl leading-5 text-blue-500 transition ease-in-out duration-700 hover:scale-110">
-          <a href="mailto:info.bubblehaven@gmail.com">
-              info.bubblehaven@gmail.com
-          </a>
-          </p>
-          <p className="font-gamja mt-4 -mb-8 text-center text-xl leading-5 text-pink-500">
+        </p>
+        <p className="font-gamja mt-5 text-center text-xl leading-5 text-blue-500 transition ease-in-out duration-700 hover:scale-110">
+          <a href="mailto:info.bubblehaven@gmail.com">info.bubblehaven@gmail.com</a>
+        </p>
+        <p className="font-gamja mt-4 text-center text-xl leading-5 text-pink-500">
           123 Bubble Lane Sipville, TeaTown 98765 BobaLand
-          </p>
+        </p>
       </div>
-      </footer>
+    </footer>
     )
 }
 
