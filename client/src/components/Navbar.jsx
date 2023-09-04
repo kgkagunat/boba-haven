8b92fde80e718bcc2871089481fa734caa253bf1
kgkagunat <kgkagunat@gmail.com>
2023-09-04 01:48:41 -1000

    modal styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -121,7 +121,7 @@ function Navbar() {
                 <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
               </div>
               <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-              <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+              <div className="inline-block align-bottom bg-gradient-to-br from-white to-pink-400 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
                 <div>
                   <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
                     <svg className="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -129,18 +129,18 @@ function Navbar() {
                     </svg>
                   </div>
                   <div className="mt-3 text-center sm:mt-5">
-                    <h3 className="text-lg leading-6 font-medium text-gray-900">
-                      Access Denied
+                    <h3 className="font-twinkle font-bold text-2xl leading-6 text-black m-3">
+                      Uh oh, can't access the cart!
                     </h3>
                     <div className="mt-2">
-                      <p className="text-sm text-gray-500">
+                      <p className="font-gamja text-2xl text-blue-600">
                         You must be logged in to access the cart.
                       </p>
                     </div>
                   </div>
                 </div>
                 <div className="mt-5 sm:mt-6">
-                  <button onClick={() => setShowModal(false)} type="button" className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm">
+                  <button onClick={() => setShowModal(false)} type="button" className="font-gamja text-2xl inline-flex justify-center w-full rounded-md border border-transparent shadow-md px-4 py-1 bg-red-600 font-bold text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 ">
                     Close
                   </button>
                 </div>
